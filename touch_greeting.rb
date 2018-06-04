252ce7f8e3fc8b813329dc972e138ff6d0cadc36
paigevogel <paigevogel36@gmail.com>
2018-06-04 23:00:47 +0000

    automatically backed up by learn

--- touch_greeting.rb
+++ touch_greeting.rb
@@ -1,8 +1,8 @@
 def greeting
  local_greeting = "Hi"
- your_name = Holly
- my_name = Paige
- time_of_day = Night
+ your_name = "Holly"
+ my_name = "Paige"
+ time_of_day = "Night"
   puts "#{local_greeting}, #{your_name}, I'm #{my_name}, how's your #{time_of_day}?"
 end 
 greeting
